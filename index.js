ceecfb0af91b8eda586b54483ee846c148626cf8
Pranjal <jainpranjal30112002@gmail.com>
2023-08-19 17:36:27 +0530

    connected MongoDB to NodeJS

--- index.js
+++ index.js
@@ -1,66 +1,94 @@
 const express = require("express");
 const app = express(); // it creates an instance
-const users = require('./MOCK_DATA.json');
 const fs = require("fs");
-
+const mongoose = require("mongoose");
 const PORT = 8000;
+
+//Connection
+mongoose
+.connect("mongodb://127.0.0.1:27017/nodeJS-app-1")
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log("Error",err));
+
+//Schema
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    jobTitle: {
+        type: String,
+    },
+    gender: {
+        type: String,
+    },
+    },
+    {
+        timestamps: true
+    }
+)
+
+const User = new mongoose.model("user", userSchema);
 
 //Middleware- can be understood as a plugin for now.
 app.use(express.urlencoded({ extended: false}));
 
 //Routes
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+    const allDbUsers = await User.find({});
     const html = `
     <ul>
-    ${users.map((user) => `<li>${user.first_name}</li>`).join('')}
+    ${allDbUsers.map((user) => `<li>${user.firstName} - ${user.email}</li>`).join('')}
     </ul>`;
     res.send(html);
 })
 
 app.route("/api/users")
-.get((req, res) => {
-    return res.json({status: "Pending"});
+.get( async (req, res) => {
+    const allDbUsers = await User.find({});
+    return res.json(allDbUsers);
 })
-.post((req, res) => {
+.post( async (req, res) => {
     const body = req.body; // all the data is in the body by default.
     if( !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title ){
         return res.status(400).json({ status: "All fields are req..."});
     }
-    users.push({...body, id: users.length + 1});
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-        return res.status(201).json({status: "Success", id: users.length});
-    })
+    // users.push({...body, id: users.length + 1});
+    // fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    //     return res.status(201).json({status: "Success", id: users.length});
+    // })
+    const result = await User.create({
+        firstName: body.first_name,
+        lastName: body.last_name,
+        email: body.email,
+        jobTitle: body.job_title,
+        gender: body.gender,
+    });
+    console.log("result ", result);
+    return res.status(201).json({status: "success"});
 })
 
 app.route("/api/users/:id")
-.get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
+.get( async (req, res) => {
+    const user = await User.findById(req.params.id);
         if(!user){
             return res.status(404).json({ error: "User Not Found!"});
         }
     return res.json(user);
 })
-.delete((req,res) => {
-    const id = Number(req.params.id);
-    const userIdx = users.findIndex((user) => user.id === id);
-    const delUser = users.splice(userIdx, 1);
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-        return res.status(200).json( {status: "success", delUser });
-    })
+.delete( async (req,res) => {
+    await User.findByIdAndDelete(req.params.id);
+    return res.json( {status: "success"});
 })
-
-// app.patch("/api/users/:id", (req,res) => {
-//     const getId = Number(req.params.id);
-//     const body = req.body;
-//     const userIdx = users.findIndex((user) => user.id === getId);
-//     const updatedUser = {...userIdx, ...body};
-//     userIdx = updatedUser;
-//     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err,data) => {
-//         return res.json({status: "Success", updatedUser});
-//     })
-// })
     
 
 app.listen(PORT, console.log(`Your server started at ${PORT}`));
